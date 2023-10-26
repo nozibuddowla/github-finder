b51ede0b159abe7d455c29129ad1da8f99690300
nozibuddowla <nozibuddowla@gmail.com>
2023-10-26 11:12:57 +0600

    fetch github repositroy list from user

--- js/script.js
+++ js/script.js
@@ -14,13 +14,30 @@ searchButton.addEventListener('click', (e) => {
             console.log(data);
             if (data.message === 'Not Found') {
                 // Show Alert
-                
+                ui.showAlert('We could not find the entered User! try again...', 'alert alert-danger');
             } else {
-                // Show Profile
-                ui.showProfile(data);
+                // Fetch user's repositories
+                fetch(`https://api.github.com/users/${userText}/repos`)
+                .then(result => {
+                    if(!result.ok) {
+                        throw new Error(`Failed to fetch repositories: ${result.statusText}`)
+                    }
+                    return result.json()
+                })
+                .then(repos => {
+                    ui.showProfile(data, repos); // Pass the 'repos' data here
+                })
+                .catch(error => {
+                    console.error('Error fetching repositories:', error);
+                    ui.showProfile(data, []); // Pass an empty array in case of an error
+                })
             }
+        })
+        .catch(error => {
+            console.error("Error fetching user data:", error);
         })
     } else {
         // Clear Profile
+        ui.clearProfile();
     }
 })
--- js/ui.js
+++ js/ui.js
@@ -3,30 +3,91 @@ class UI {
         this.profile = document.querySelector('#profile');
     }
 
-    showProfile(user) {
+    showProfile(user, repos) {
+        this.clearAlert();
         this.profile.innerHTML = `
             <div class="card card-body mb-3">
                 <div class="row">
                     <div class="col-md-3">
                         <img src="${user.avatar_url}" alt="${user.name}" class="img-fluid mb-2">
-                        <a href="${user.html_url}" target="_blank" class="btn bton-primary btn-block mb-4">View Profile</a>
+                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
                     </div>
 
                     <div class="col-md-9">
-                        <span class="badge rounde-pill text-bg-primary">public Repositry: ${user.public_repos}</span>
-                        <span class="badge rounde-pill text-bg-secondary">Public Gists: ${user.public_gists}</span>
-                        <span class="badge rounde-pill text-bg-success">Followers: ${user.followers}</span>
-                        <span class="badge rounde-pill text-bg-info">Following: ${user.following}</span>
+                        <span class="badge round-pill text-bg-primary">public Repositry: ${user.public_repos}</span>
+                        <span class="badge round-pill text-bg-secondary">Public Gists: ${user.public_gists}</span>
+                        <span class="badge round-pill text-bg-success">Followers: ${user.followers}</span>
+                        <span class="badge round-pill text-bg-info">Following: ${user.following}</span>
                         <br></br>
                         <ul class="list-group">
-                            <li class="list-group-item">Company: ${user.company}</li>
-                            <li class="list-group-item">Website/Blog: ${user.blog}</li>
-                            <li class="list-group-item">Locations: ${user.location}</li>
-                            <li class="list-group-item">Member Since: ${user.created_at}</li>
+                            <li class="list-group-item card-text">Company: ${user.company}</li>
+                            <li class="list-group-item card-text">Website/Blog: ${user.blog}</li>
+                            <li class="list-group-item card-text">Locations: ${user.location}</li>
+                            <li class="list-group-item card-text">Member Since: ${user.created_at}</li>
                         </ul>
                     </div>
                 </div>
             </div>
+            <h3 class="page-heading mb-3">Latest Repositories</h3>
+            <div id='repos' class="row row-cols-1 row-cols-3 g-4">
+
+            </div>
         `;
+        this.showRepos(repos);
+    }
+
+    showRepos(repos) {
+        let output = '';
+        console.log(output);
+        if (Array.isArray(repos)) {
+            repos.forEach(repo => {
+                output += `
+                <div class='col'>
+                    <div class='card'>
+                        <div class="card-body mb-2">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h5 class='card-title'>
+                                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                                    </h5>
+                                    
+                                </div>
+                                <div class="col-md-6">
+                                    <span class="badge round-pill text-bg-primary">Stars: ${repo.stargazers_count}</span>
+                                    <span class="badge round-pill text-bg-secondary">Watchers: ${repo.watchers_count}</span>
+                                    <span class="badge round-pill text-bg-success">Forks: ${repo.forks_count}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                `
+            });
+            document.querySelector('#repos').innerHTML = output;
+        }
+        
+    }
+
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
+
+    showAlert(message, className) {
+        this.clearAlert();
+        this.clearProfile();
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.searchContainer');
+        const search = document.querySelector('.search');
+        container.insertBefore(div, search);
+    }
+
+    clearAlert() {
+        let currentAlert = document.querySelector('.alert');
+        if (currentAlert) {
+            currentAlert.remove();
+        }
     }
 }
